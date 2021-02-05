0ae5ef06b441984952fad0156dc93ad48c7a5053
WilderGitHub <wilder_serdan@hotmail.com>
2021-02-05 14:01:37 -0400

    mas comentarios

--- app.py
+++ app.py
@@ -3,40 +3,33 @@ import pandas as pd
 #import pyxlsb
 #aqui ver la extensión del archivo y que abra si cambiaron de formato
 archivoAduanas = "assets/aduanasDiciembre2020.xls"
-#CamposPaTrabajar = ['Tramite': 'tramite','[Doc. Export]':'idExportador','Exportador': 'exportador','[Importe Factura]': 'factura','[Posición Arancelaria]': 'NANDINA','FOB $us': 'fob','Sector': 'sector']
+#con estos campos vamos a trabajar
 CamposPaTrabajar = ['tramite','idExportador','exportador','factura','NANDINA','fob','sector']
-#aqui ver la extensión del archivo y que abra si cambiaron de formato
+#tal vez volverlo matriz json para no tener que manejar muchos exceles
 diccionario = 'diccionario.xlsx'
-#eso podemos utilizar para los parametros
-#PalabrasClave = 'PalabrasClave.xlsx'
-#agarramos el parametro "archivitos" y les hacemos Dataframes y concatenamos
+
+#cargamos el archivo de aduanas
 dfArchivo =pd.read_excel(archivoAduanas)#, engine='pyxlsb')
+#seleccionamos solo las columnas relevantes
 dfArchivoColumnas= dfArchivo.iloc[:,[0,5,6,8,11,14,15]]
+#generamos un dataframe con los nombres mas cortos oe
 dfArchivoColumnas.columns = CamposPaTrabajar    
-print("este hay que transformar")
-print(dfArchivoColumnas.head)
-#dfArchivoColumnas['NANDINAnuevo']=dfArchivoColumnas.NANDINA.map(lambda x:x/10)
-print("NANDINA")
-print(dfArchivoColumnas['NANDINA']) 
+
+#transformamos la columna para que sea comparable con el diccionario
 dfArchivoColumnas['NANDINA']=dfArchivoColumnas['NANDINA'].transform(lambda x:x*0.1)
-print("NANDINA transformado")
-print(dfArchivoColumnas['NANDINA']) 
+
 dfArchivoParaTrabajar = dfArchivoColumnas
-print("ArchivoParaTrabajar a ver")
-print(dfArchivoParaTrabajar.head)  
-print(dfArchivoParaTrabajar.columns)  
-###print(dfDatosPaTrabajar.columns)  
+
 #Seleccionamos los campos del diccionario diccionario que nos interesa
 dfdiccionarioP = pd.read_excel(diccionario)
 print(dfdiccionarioP.head) 
 dfdiccionario = pd.DataFrame(dfdiccionarioP, columns=['NANDINA','COD','PRODUCTO'])
 print(dfdiccionario.columns)  
-valorPorDefecto= "Por identificar"
-#Juntamos nuestro Dataframe con Naturalezas tipo BuscarV
+valorPorDefecto= "No es minerales"
+#Juntamos nuestro Dataframe con el diccionario tipo BuscarV
 resultadoMerge=dfArchivoParaTrabajar.merge(dfdiccionario, on='NANDINA', how='left').fillna(valorPorDefecto)
 print("resultado merge")
-print(resultadoMerge.head)
-print(resultadoMerge.columns)
-###dfDatosPaTrabajar['Naturaleza']=resultadoMerge['Naturaleza']
+#exportamos a excel
+
 with pd.ExcelWriter('hola.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
     resultadoMerge.to_excel(writer, sheet_name='Resumena') 
