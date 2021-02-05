e2f102a469e5d962dbc2cf20d4aa91308232b5b6
WilderGitHub <wilder_serdan@hotmail.com>
2021-02-05 13:40:03 -0400

    ya funcion el merfe (vlookup, buscarv con el dicionario

--- app.py
+++ app.py
@@ -13,20 +13,27 @@ diccionario = 'diccionario.xlsx'
 dfArchivo =pd.read_excel(archivoAduanas)#, engine='pyxlsb')
 dfArchivoColumnas= dfArchivo.iloc[:,[0,5,6,8,11,14,15]]
 dfArchivoColumnas.columns = CamposPaTrabajar    
-dfArchivoColumnas['NANDINAnuevo']=dfArchivoColumnas.NANDINA.map(lambda x:x/10)
+print("este hay que transformar")
+print(dfArchivoColumnas.head)
+#dfArchivoColumnas['NANDINAnuevo']=dfArchivoColumnas.NANDINA.map(lambda x:x/10)
+print("NANDINA")
+print(dfArchivoColumnas['NANDINA']) 
+dfArchivoColumnas['NANDINA']=dfArchivoColumnas['NANDINA'].transform(lambda x:x*0.1)
+print("NANDINA transformado")
+print(dfArchivoColumnas['NANDINA']) 
 dfArchivoParaTrabajar = dfArchivoColumnas
-print("ArchivoParaTrabajar")
+print("ArchivoParaTrabajar a ver")
 print(dfArchivoParaTrabajar.head)  
 print(dfArchivoParaTrabajar.columns)  
 ###print(dfDatosPaTrabajar.columns)  
 #Seleccionamos los campos del diccionario diccionario que nos interesa
 dfdiccionarioP = pd.read_excel(diccionario)
 print(dfdiccionarioP.head) 
-dfdiccionario = pd.DataFrame(dfdiccionarioP, columns=['NANDINA','COD'])
+dfdiccionario = pd.DataFrame(dfdiccionarioP, columns=['NANDINA','COD','PRODUCTO'])
 print(dfdiccionario.columns)  
 valorPorDefecto= "Por identificar"
 #Juntamos nuestro Dataframe con Naturalezas tipo BuscarV
-resultadoMerge=dfArchivoParaTrabajar.merge(dfdiccionario, on='NANDINAnuevo', how='left').fillna(valorPorDefecto)
+resultadoMerge=dfArchivoParaTrabajar.merge(dfdiccionario, on='NANDINA', how='left').fillna(valorPorDefecto)
 print("resultado merge")
 print(resultadoMerge.head)
 print(resultadoMerge.columns)
