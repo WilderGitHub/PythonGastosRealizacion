9b3c31ee3b02874976fe61d0c29a6a8c5c13afb4
WilderGitHub <wilder_serdan@hotmail.com>
2021-02-08 16:11:13 -0400

    comentarios mas ordenados

--- app.py
+++ app.py
@@ -7,19 +7,15 @@ archivoAduanas = "assets/aduanasDiciembre2020.xls"
 CamposPaTrabajar = ['tramite','idExportador','exportador','factura','NANDINA','fob','sector']
 #tal vez volverlo matriz json para no tener que manejar muchos exceles
 diccionario = 'diccionario.xlsx'
-
 #cargamos el archivo de aduanas
 dfArchivo =pd.read_excel(archivoAduanas)#, engine='pyxlsb')
 #seleccionamos solo las columnas relevantes
 dfArchivoColumnas= dfArchivo.iloc[:,[0,5,6,8,11,14,15]]
 #generamos un dataframe con los nombres mas cortos oe
 dfArchivoColumnas.columns = CamposPaTrabajar    
-
 #transformamos la columna para que sea comparable con el diccionario
 dfArchivoColumnas['NANDINA']=dfArchivoColumnas['NANDINA'].transform(lambda x:x*0.1)
-
 dfArchivoParaTrabajar = dfArchivoColumnas
-
 #Seleccionamos los campos del diccionario diccionario que nos interesa
 dfdiccionarioP = pd.read_excel(diccionario)
 print(dfdiccionarioP.head) 
@@ -30,6 +26,5 @@ valorPorDefecto= "No es minerales"
 resultadoMerge=dfArchivoParaTrabajar.merge(dfdiccionario, on='NANDINA', how='left').fillna(valorPorDefecto)
 print("resultado merge")
 #exportamos a excel
-
 with pd.ExcelWriter('hola.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
     resultadoMerge.to_excel(writer, sheet_name='Resumena') 
